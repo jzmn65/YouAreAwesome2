10679f2b1f93cb514f4868f235030a586392287e
Jazmine Singh <jzmn65@gmail.com>
2025-08-31 10:50:20 -0400

    First commit of YouAreAwesome after updating to IOS 15. Solutions from chap 1.3 in Swift UI Course

--- YouAreAwesome2/ContentView.swift
+++ YouAreAwesome2/ContentView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "You Are Awsome!"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+            Spacer()
+            HStack {
+                Button("Awesome!"){
+                    message = "Awesome!"
+                }
+                Button("Great!"){
+                    message = "Awesome!"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
+            
         }
         .padding()
     }
