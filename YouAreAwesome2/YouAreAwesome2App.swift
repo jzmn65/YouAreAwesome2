8ec65525db3a2896d1b167ce7c4df56423cdb85e
Jazmine Singh <jzmn65@gmail.com>
2025-08-31 10:26:14 -0400

    Initial Commit

---
+++ YouAreAwesome2/YouAreAwesome2App.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesome2App.swift
+//  YouAreAwesome2
+//
+//  Created by Jazmine Singh on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesome2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YouAreAwesome2Tests/YouAreAwesome2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesome2Tests.swift
+//  YouAreAwesome2Tests
+//
+//  Created by Jazmine Singh on 8/31/25.
+//
+
+import Testing
+@testable import YouAreAwesome2
+
+struct YouAreAwesome2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
