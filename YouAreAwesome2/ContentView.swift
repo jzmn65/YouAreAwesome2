2b3aa99b6fba024e23949d762d0540c018c4b94c
Jazmine Singh <jzmn65@gmail.com>
2025-09-07 15:15:43 -0400

    Chapter 1.11 commit

--- YouAreAwesome2/ContentView.swift
+++ YouAreAwesome2/ContentView.swift
@@ -38,27 +38,22 @@ struct ContentView: View {
                                     "You Are Great!",
                                     "You Are Fantastic",
                                     "Fabulous? That's You!"]
-                    message = messages[messageNumber]
-                    messageNumber += 1
-                    if messageNumber == messages.count {
-                        messageNumber = 0
-                    }
-                    
-                    imageName = "image\(imageNumber)"
-                    imageNumber += 1
-                    if imageNumber > 9{
-                        imageNumber = 0
-                    }
+                    //
+                    message = messages[Int.random(in: 0...messages.count-1)]
+                    imageName = "image\(Int.random(in: 0...9))"
                 }
+                
             }
-            .buttonStyle(.borderedProminent)
-            .font(.title2)
-
-            
         }
         .padding()
+        .buttonStyle(.borderedProminent)
+        .font(.title2)
     }
-}
+            
+        }
+    
+    
+
 
 #Preview {
     ContentView()
